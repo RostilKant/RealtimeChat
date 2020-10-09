64f942dfb4b19f1477417c6f38d1512f757d713c
RostilKant <rostilnytskiy@gmail.com>
2020-10-09 19:52:46 +0300

    Configuring CORS & enabling SignalR

--- RealtimeChat/Startup.cs
+++ RealtimeChat/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Entities.HubConfig;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +26,16 @@ namespace RealtimeChat
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+                options.AddPolicy("CorsPolicy", builder => 
+                    builder
+                        .AllowCredentials()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .WithOrigins("http://localhost:4200")
+                    ));
+            services.AddSignalR();
+            
             services.AddControllers();
         }
 
@@ -42,7 +53,11 @@ namespace RealtimeChat
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chat");
+            });
         }
     }
 }
